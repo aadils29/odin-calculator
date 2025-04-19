b15d1f66cf8425b274328a570519022480771eef
aadils <aadilsingh@outlook.com>
2025-04-19 17:25:44 -0400

    Set up Operation variables and operate function
    Operate function decides which operation to execute based on the user's
    input. The variables that were added are for the two numbers that
    are being operated on

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let displayValue = "0"; // default value on calc screen
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +17,25 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) {
+        return "Error";
+      }
+      return divide(a, b);
+    default:
+      return "Error";
+  }
 }
